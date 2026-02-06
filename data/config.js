d7c5d9efaf54359c05ef52f847695c53f531f62d
mahmudialamsyah <mahmudialamsyah@gmail.com>
2026-02-06 12:33:36 +0700

    Create config.js

---
+++ data/config.js
@@ -0,0 +1,16 @@
+// data/config.js
+categoryConfig = [
+    { id: 'agama', label: 'Agama', cls: 'cat-1' },
+    { id: 'indo', label: 'Bahasa Indonesia', cls: 'cat-2' },
+    { id: 'english', label: 'English', cls: 'cat-3' },
+    { id: 'ipa', label: 'IPA', cls: 'cat-4' },
+    { id: 'math', label: 'Matematika', cls: 'cat-5' },
+    { id: 'seni', label: 'Seni Budaya', cls: 'cat-6' },
+    { id: 'geo', label: 'Geografi', cls: 'cat-7' },
+    { id: 'sejarah', label: 'Sejarah', cls: 'cat-8' },
+    { id: 'dunia', label: 'Dunia', cls: 'cat-9' },
+    { id: 'others', label: 'Others', cls: 'cat-10' },
+    { id: 'random', label: 'Random', cls: 'cat-11' }
+    // Contoh menambah kategori baru:
+    // { id: 'tik', label: 'TIK', cls: 'cat-1' }
+];
